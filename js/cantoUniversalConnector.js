4a998cb1a507eefd6f0441f2b50fcaa573cb6532
Levi Gao <levigao@nagarro.com>
2022-02-22 16:33:22 +0800

    no message

--- js/cantoUniversalConnector.js
+++ js/cantoUniversalConnector.js
@@ -7,19 +7,20 @@
     pluginName = "cantoUC",
     redirectUri = "",
     tokenInfo = {},
-    env = "flightbycanto.com",  //flightbycanto.com/staging.cantoflight.com/canto.com/canto.global/canto.de/cantodemo.com
-    appId = "f5ecd6095ebb469691b7398e4945eb44",
+    env = "canto.com",  //flightbycanto.com/staging.cantoflight.com/canto.com/canto.global/canto.de/cantodemo.com
+    appId = "a9dc81b1bf9d492f8ee3838302d266b2",
     _workerID = "",
     callback,
     currentCantoTagID,
     formatDistrict;
     var secretObj = {
-            "flightbycanto.com":"4610e1613b444a28bd5329784224a8ba2e725e6664744dd4ad405643c0d1eb84",
-            "staging.cantoflight.com":"114de1921619437a954a41bc5a5c99bb4d6ac397f6514f009c0b922fb8169ace",
-            "canto.com":"585070cc17ea463390e9224717dbdd85f2a2ff165ebb4083bbc7ff60f4f2e873",
-            "canto.global":"1d902fb7bf9843d99a7280fb660f35257ff75ffd41ed44bf8f759021bad24c44",
-            "canto.de":"3a093e70635c4a418f9e7e13d936d28f8250f2ca3a6d45c989b010c197bb27cf",
-            "cantodemo.com":"4a284948051449d485cb77b41bba86cf964cef9e4a9945d0a6d9e2714d9bf409",
+        // "flightbycanto.com":"4610e1613b444a28bd5329784224a8ba2e725e6664744dd4ad405643c0d1eb84",
+        // "staging.cantoflight.com":"114de1921619437a954a41bc5a5c99bb4d6ac397f6514f009c0b922fb8169ace",
+        "canto.com":"585070cc17ea463390e9224717dbdd85f2a2ff165ebb4083bbc7ff60f4f2e873",
+        "canto.global":"1d902fb7bf9843d99a7280fb660f35257ff75ffd41ed44bf8f759021bad24c44",
+        "canto.de":"3a093e70635c4a418f9e7e13d936d28f8250f2ca3a6d45c989b010c197bb27cf",
+        "ca.canto.com":"3a093e70635c4a418f9e7e13d936d28f8250f2ca3a6d45c989b010c197bb27cf",
+        // "cantodemo.com":"4a284948051449d485cb77b41bba86cf964cef9e4a9945d0a6d9e2714d9bf409",
     };
 
     cantoUC = $.fn[pluginName] = $[pluginName] = function (options, callback) {
@@ -31,7 +32,7 @@
         createIframe();
         addEventListener();
         //initCantoTag();
-      
+
         window.onmessage=function(e){
             var data = event.data;
             if(data && data.type == "getTokenInfo"){
@@ -44,7 +45,7 @@
                      url: Drupal.url('canto_connector/delete_access_token'),
                      type: 'POST',
                      data: { 'accessToken': tokenInfo.accessToken, 'env': env
-                     	
+
                      },
                      dataType: 'json',
                    });
@@ -52,7 +53,7 @@
                 //clear token and close the frame.
                 tokenInfo = {};
                 $(".canto-uc-iframe-close-btn").trigger("click");
-    
+
             } else if(data && data.type == "cantoInsertImage"){
                 $(".canto-uc-iframe-close-btn").trigger("click");
                 // insertImageToCantoTag(cantoURL);
@@ -70,17 +71,18 @@
                 var cantoContentPage = "https://s3-us-west-2.amazonaws.com/static.dmc/universal/cantoContent.html";
                 $("#cantoUCFrame").attr("src", cantoContentPage);
             }
-    
+
         };
     };
     function settings(options){
         var envObj = {
-            "flightbycanto.com":"f5ecd6095ebb469691b7398e4945eb44",
-            "staging.cantoflight.com":"f18c8f3b79644b168cad5609ff802085",
+            // "flightbycanto.com":"f5ecd6095ebb469691b7398e4945eb44",
+            // "staging.cantoflight.com":"f18c8f3b79644b168cad5609ff802085",
             "canto.com":"a9dc81b1bf9d492f8ee3838302d266b2",
             "canto.global":"f87b44d366464dfdb4867ab361683c96",
             "canto.de":"e7135823e3d046468287e835008da493",
-            "cantodemo.com":"de5c606732a34b44b99ec20c40f6cb5e",
+            "ca.canto.com":"e7135823e3d046468287e835008da493",
+            // "cantodemo.com":"de5c606732a34b44b99ec20c40f6cb5e",
         };
         env = options.env;
         appId = envObj[env];
@@ -108,17 +110,17 @@
         })
         .on('click', ".js-canto-oauth-btn", function(e){
             var getSSOcodeURL = "https://oauth." + env + "/oauth/api/oauth2/ssocode?worker_id=" + _workerID;
-            $.ajax({  
+            $.ajax({
 	            type: "GET",
 	            url: getSSOcodeURL,
-	            async: false,  
+	            async: false,
 	            error: function(request) {
                     alert("Get login url failed, please retry");
                     $("#cantoUCPanel").addClass("hidden");
-                },  
+                },
                 success: function(data) {
                     getTokenByOauthCode(data);
-                } 
+                }
             });
         });
     }
@@ -136,36 +138,36 @@
             url: url,
             timeout: 8000
           };
-        $.ajax({  
+        $.ajax({
             type: "POST",
             url: url,
-            async: false,  
+            async: false,
             data: data,
             error: function(request) {
                 alert("Get login url failed, please retry");
                 $("#cantoUCPanel").addClass("hidden");
-            },  
+            },
             success: function(data) {
                 // console.log(data);
                 getTenant(data)
-            } 
+            }
         });
     }
     function getTenant(tokens){
         var url = "https://oauth." + env + ":443/oauth/api/oauth2/tenant/" + tokens.refreshToken;
-        $.ajax({  
+        $.ajax({
             type: "GET",
             url: url,
-            async: false,  
+            async: false,
             error: function(request) {
                 alert("Get login url failed, please retry");
                 $("#cantoUCPanel").addClass("hidden");
-            },  
+            },
             success: function(data) {
                 // console.log(data);
                 tokens.refreshToken = data;
                 saveTokenToDrupal(tokens);
-            } 
+            }
         });
     }
     function saveTokenToDrupal(tokens){
@@ -185,13 +187,12 @@
         var body = $("body");
         var cantoTag = body.find("canto");
         var imageHtml = '<button class="canto-pickup-img-btn">+ Insert Files from Canto</button>';
-    
+
         cantoTag.append(imageHtml);
     }
 
     /*--------------------------load iframe content---------------------------------------*/
     function loadIframeContent() {
-
         var cantoLoginPage = "https://oauth." + env + "/oauth/api/oauth2/compatible/authorize?response_type=code&app_id=" + appId + "&redirect_uri=http://loacalhost:3000&state=abcd";
 
          var cantoContentPage = "https://s3-us-west-2.amazonaws.com/static.dmc/universal/cantoContent.html";
@@ -201,15 +202,15 @@
             $("#cantoUCFrame").removeClass("hidden");
         } else {
             var getSSOwidURL = "https://oauth." + env + "/oauth/api/oauth2/ssowid";
-            $.ajax({  
+            $.ajax({
 	            type: "GET",
 	            url: getSSOwidURL,
 	            data:$('#frmGrant').serialize(),
-	            async: false,  
+	            async: false,
 	            error: function(request) {
                     alert("Get login url failed, please retry");
                     $("#cantoUCPanel").addClass("hidden");
-                },  
+                },
                 success: function(data) {
                     _workerID = data;
                     cantoLoginPage += "&worker_id=" + data;
@@ -217,9 +218,9 @@
                     window.open(cantoLoginPage, '_blank');
                     $(".canto-sso-section").removeClass("hidden");
                     $("#cantoUCFrame").addClass("hidden");
-                } 
+                }
             });
-            
+
         }
     }
     /*--------------------------add iframe---------------------------------------*/
@@ -238,7 +239,7 @@
         iframeHtml += '</div>';
         iframeHtml += '<iframe id="cantoUCFrame" class="canto-uc-subiframe hidden" src=""></iframe>';
         iframeHtml += '</div>';
-        
+
         body.append(iframeHtml);
     }
     /*--------------------------load file---------------------------------------*/
